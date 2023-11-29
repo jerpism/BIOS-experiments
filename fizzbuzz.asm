91af78971f5255f6f6ff83d8746212fa94dc0868
Jerpism <jere.m@live.fi>
2023-11-29 19:47:19 +0200

    Add pageup and pagedown functionality to fizzbuzz

--- fizzbuzz.asm
+++ fizzbuzz.asm
@@ -6,11 +6,15 @@
 ; to something lower like 25 might be preferable
 
 ; assemble with `nasm -f bin -o fizzbuzz.o fizzbuzz.asm`
+; the number we should count up to 
 %define MAX_NUM 100
+
+; How many lines to display per page
+%define MAX_LINES 25
 
 [org 0x7c00]
     ; set up stack at just below what we load
-    ; and all segment registers to 0
+    ; and segment registers to 0
     cli
     mov     bp, 0x7c00
     xor     ax, ax
@@ -20,16 +24,23 @@
     mov     sp, bp
     sti
 
-    jmp     fizzbuzz
+    jmp     setmode
+
+setmode:
+    mov     ah, 0x00    ; set video mode
+    mov     al, 0x02    ; 80x25 greyscale
+    int     0x10
 
 fizzbuzz:
     mov     cx, 0       ; counter initialize
 
     .loop:
     add     cx, 1       
-    cmp     cx, MAX_NUM ; print out to this number
-    jg      .end        ; done
+    cmp     cx, MAX_NUM ; got to the end?
+    jg      .end        ; yes, we're done
 
+    call    check_page  ; see if we're good to print on this page
+                        ; or if we need to move to a new one
 
     mov     ax, cx      ; counter value to ax for div
     xor     dx, dx      ; clear dx
@@ -83,9 +94,86 @@ fizzbuzz:
 
 
     .end:
+    jmp kbin   ; go to reading keyboard, unnecessary for now since it falls right in
 
-    jmp kbin   ; just sit here forever
 
+; Will block and wait for key to be read in
+; Program doesn't do anything else at the moment so this is fine
+;
+; PGUP to go to previous page
+; PGDN to go to next page
+;
+; Note that there's currently nothing limiting going past
+; the last page if PGDN is pressed
+kbin:
+    mov     ah, 0x00    ; read key
+    int     0x16        
+
+    cmp     ah, 0x49    ; pgup code?
+    je      .pgup       ; go handle that
+
+    cmp     ah, 0x51    ; pgdn scan code?
+    jne     kbin        ; no, skip back up
+                        ; otherwise fall thru
+
+    .pgdn:
+    mov     ah, 0x0f    ; get video mode
+    int     0x10
+
+    mov     al, bh      ; get current page n to AL
+    add     al, 1       ; we'll want page n+1
+    mov     ah, 0x05    ; and set the current one
+    int     0x10        ; to that page
+    jmp kbin            ; wait for next key
+
+    .pgup:
+    mov     ah, 0x0f    ; get video mode
+    int     0x10
+
+    mov     al, bh      ; get current page n to AL
+    sub     al, 1       ; we'll want page n-1
+    mov     ah, 0x05    ; and set the current one
+    int     0x10        ; to that page
+    jmp kbin            ; wait for next key
+
+
+; Checks counter to see if we're
+; good to keep using the current page
+; or if we should move one ahead.
+
+; Uses value in CX to determine this
+; Change macro MAX_LINES as desired
+check_page:
+    cmp     cx, MAX_LINES   ; if cx < MAX_LINES
+    jb      .below_maxl     ; just return
+
+    push    ax
+    push    bx
+    push    dx
+
+    mov     ax, cx          ; set up division
+    mov     bx, MAX_LINES
+
+    div     bx              ; ax % MAX_LINES
+    test    dx, dx          ; == 0?
+    jnz     .end            ; no, just return
+
+    mov     ah, 0x0f        ; otherwise get video mode
+    int     0x10
+
+    mov     al, bh          ; get page # to AL for our call
+    add     al, 1           ; and we'll want n+1 
+
+    mov     ah, 0x05        ; and set it as our active
+    int     0x10            ; display page
+
+    .end:
+    pop     dx
+    pop     bx
+    pop     ax
+    .below_maxl:
+    ret
+    
 
 ; use NUL terminated strings
 FIZZ_MSG:       db "Fizz", 0
